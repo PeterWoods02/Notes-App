fec8234e8a5cefc036c7d1d6454291ff07169363
20098630 <woodspeter37@gmail.com>
2023-02-09 15:29:18 +0000

    Implemented scanner input utility

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,5 @@
+import utils.ScannerInput
 import java.lang.System.exit
-import java.util.*
-
-
-val scanner = Scanner(System.`in`)
 
 fun main(args: Array<String>) {
     runMenu()
@@ -11,7 +8,7 @@ fun main(args: Array<String>) {
 
 
 fun mainMenu() : Int {
-    print(""" 
+    return ScannerInput.readNextInt(""" 
          > ----------------------------------
          > |        NOTE KEEPER APP         |
          > ----------------------------------
@@ -24,7 +21,6 @@ fun mainMenu() : Int {
          > |   0) Exit                      |
          > ----------------------------------
          > ==>> """.trimMargin(">"))
-    return scanner.nextInt()
 }
 
 
