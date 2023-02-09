d4d5343e071d2567ed9840f7cab2a9deeb0d1862
20098630 <woodspeter37@gmail.com>
2023-02-09 15:33:09 +0000

    added gradle logging

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import mu.KotlinLogging
 import utils.ScannerInput
 import java.lang.System.exit
 
@@ -6,6 +7,7 @@ fun main(args: Array<String>) {
 }
 
 
+private val logger = KotlinLogging.logger {}
 
 fun mainMenu() : Int {
     return ScannerInput.readNextInt(""" 
@@ -40,19 +42,20 @@ fun runMenu() {
 
 
 fun addNote(){
-    println("You chose Add Note")
+    logger.info { "addNote() function invoked" }
 }
 
 fun listNotes(){
-    println("You chose List Notes")
+    logger.info { "listNotes() function invoked" }
+
 }
 
 fun updateNote(){
-    println("You chose Update Note")
+    logger.info { "updateNote() function invoked" }
 }
 
 fun deleteNote(){
-    println("You chose Delete Note")
+    logger.info { "deleteNote() function invoked" }
 }
 
 fun exitApp(){
